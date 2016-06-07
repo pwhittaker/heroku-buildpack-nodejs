2cec6cb15bfb2577734dc9c0118e2ed0ca8b4d44
pwhittaker <phil@pjwweb.co.uk>
2016-06-07 16:34:24 +0100

    Update nodejs.sh

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -28,8 +28,8 @@ detect_memory() {
   esac
 }
 
-export PATH="$HOME/.heroku/node/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
-export NODE_HOME="$HOME/.heroku/node"
+export PATH="$HOME/editor/.heroku/node/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
+export NODE_HOME="$HOME/editor/.heroku/node"
 export NODE_ENV=${NODE_ENV:-production}
 
 calculate_concurrency
